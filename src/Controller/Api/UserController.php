664faba5b117ab2e0162ac110f1c9cad4895bf25
Killian <killian2908@gmail.com>
2024-09-13 09:24:42 +0200

    Update pre-token

--- src/Controller/Api/UserController.php
+++ src/Controller/Api/UserController.php
@@ -3,9 +3,15 @@
 namespace App\Controller\Api;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController {
 
     public function __construct() {}
+
+
+    #[Route('api/recipes', name: 'app_api_all_recipes')]
+    public function index()
+    {}
 
 }
